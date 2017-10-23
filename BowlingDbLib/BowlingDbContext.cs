bef0f09b8512486aeb73e8ab9214e7ee197e1bf3
aeklundh <eklundh.albin@gmail.com>
2017-10-23 12:01:27 +0200

    add bowling db lib

---
+++ BowlingDbLib/BowlingDbContext.cs
@@ -0,0 +1,22 @@
+﻿using AccountabilityLib.Classes;
+using BowlingLib;
+using Microsoft.EntityFrameworkCore;
+
+namespace BowlingDbLib
+{
+    public class BowlingDbContext : DbContext
+    {
+        public DbSet<Accountability> Accountabilities { get; set; }
+        public DbSet<AccountabilityType> AccountabilityTypes { get; set; }
+        public DbSet<Competition> Competitions { get; set; }
+        public DbSet<Lane> Lanes { get; set; }
+        public DbSet<Match> Matches { get; set; }
+        public DbSet<Party> Parties { get; set; }
+        public DbSet<Round> Rounds { get; set; }
+        public DbSet<Series> Series { get; set; }
+        public DbSet<TimePeriod> TimePeriods { get; set; }
+
+        public BowlingDbContext(DbContextOptions<BowlingDbContext> options) : base(options)
+        { }
+    }
+}
---
+++ BowlingDbLib/SqlPartyRepository.cs
@@ -0,0 +1,68 @@
+﻿using AccountabilityLib.Interfaces;
+using System;
+using AccountabilityLib.Classes;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BowlingDbLib
+{
+    public class SqlPartyRepository : IPartyRepository
+    {
+        private readonly BowlingDbContext _context;
+
+        public SqlPartyRepository(BowlingDbContext context)
+        {
+            _context = context;
+        }
+
+        public async void CreateAccountability(Party commissioner, Party responsible, AccountabilityType type, DateTime? start = null, DateTime? end = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Party> CreateParty(string name, string legalId)
+        {
+            Party retVal = new Party() { Name = name, LegalId = legalId };
+            _context.Parties.Add(retVal);
+            await _context.SaveChangesAsync();
+            return retVal;
+        }
+
+        public async Task<ICollection<Party>> GetAllParties()
+        {
+            return await _context.Parties.ToListAsync();
+        }
+
+        public async Task<ICollection<Party>> GetCommissioners(Party responsible, AccountabilityType type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Party> GetParty(string legalId)
+        {
+            return await _context.Parties.SingleOrDefaultAsync(x => x.LegalId == legalId);
+        }
+
+        public async Task<Party> GetParty(int partyId)
+        {
+            return await _context.Parties.SingleOrDefaultAsync(x => x.PartyId == partyId);
+        }
+
+        public async Task<ICollection<Party>> GetResponsible(Party commissioner, AccountabilityType type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<TimePeriod> GetTimePeriod(int timePeriodId)
+        {
+            return await _context.TimePeriods.SingleOrDefaultAsync(x => x.TimePeriodId == timePeriodId);
+        }
+
+        public async Task<TimePeriod> SearchForPeriod(DateTime startTime, DateTime endTime)
+        {
+            return await _context.TimePeriods.Where(x => x.StartTime == startTime && x.EndTime == endTime).FirstOrDefaultAsync();
+        }
+    }
+}
