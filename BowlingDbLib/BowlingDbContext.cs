1d624eaf7802791d4b84758225b016d6778cbd9f
aeklundh <eklundh.albin@gmail.com>
2017-10-23 12:16:55 +0200

    made dbsets virtual to allow mocking

--- BowlingDbLib/BowlingDbContext.cs
+++ BowlingDbLib/BowlingDbContext.cs
@@ -6,15 +6,15 @@ namespace BowlingDbLib
 {
     public class BowlingDbContext : DbContext
     {
-        public DbSet<Accountability> Accountabilities { get; set; }
-        public DbSet<AccountabilityType> AccountabilityTypes { get; set; }
-        public DbSet<Competition> Competitions { get; set; }
-        public DbSet<Lane> Lanes { get; set; }
-        public DbSet<Match> Matches { get; set; }
-        public DbSet<Party> Parties { get; set; }
-        public DbSet<Round> Rounds { get; set; }
-        public DbSet<Series> Series { get; set; }
-        public DbSet<TimePeriod> TimePeriods { get; set; }
+        public virtual DbSet<Accountability> Accountabilities { get; set; }
+        public virtual DbSet<AccountabilityType> AccountabilityTypes { get; set; }
+        public virtual DbSet<Competition> Competitions { get; set; }
+        public virtual DbSet<Lane> Lanes { get; set; }
+        public virtual DbSet<Match> Matches { get; set; }
+        public virtual DbSet<Party> Parties { get; set; }
+        public virtual DbSet<Round> Rounds { get; set; }
+        public virtual DbSet<Series> Series { get; set; }
+        public virtual DbSet<TimePeriod> TimePeriods { get; set; }
 
         public BowlingDbContext(DbContextOptions<BowlingDbContext> options) : base(options)
         { }
