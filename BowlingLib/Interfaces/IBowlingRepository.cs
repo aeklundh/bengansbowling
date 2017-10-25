1027b5cb0eca2f9cb96798d9f8e6822c00c05664
aeklundh <eklundh.albin@gmail.com>
2017-10-25 09:25:41 +0200

    add basic bowling repository interface

---
+++ BowlingLib/Interfaces/IBowlingRepository.cs
@@ -0,0 +1,22 @@
+﻿using AccountabilityLib.Models;
+using BowlingLib.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BowlingLib.Interfaces
+{
+    public interface IBowlingRepository
+    {
+        Task<Competition> GetCompetition(int competitionId);
+        Task<Match> GetMatch(int matchId);
+        Task<Round> GetRound(int roundId);
+        Task<Series> GetSeries(int seriesId);
+        Task<ICollection<Lane>> GetAllLanes();
+        Task<Lane> GetLane(int laneId);
+
+        Task<Competition> CreateCompetition(string name);
+        Task<Match> CreateEmptyMatch();
+        Task<Match> AddMatch(Match match);
+        Task<Round> CreateRound(Match match);
+    }
+}
