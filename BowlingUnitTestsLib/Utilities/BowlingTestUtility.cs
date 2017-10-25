b56497c3c5b8b08c3db64482c74d75ff57af9391
aeklundh <eklundh.albin@gmail.com>
2017-10-25 18:31:35 +0200

    add utility with method for creating sample matches

---
+++ BowlingUnitTestsLib/Utilities/BowlingTestUtility.cs
@@ -0,0 +1,34 @@
+﻿using AccountabilityLib.Models;
+using BowlingLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingUnitTestsLib.Utilities
+{
+    public static class BowlingTestUtility
+    {
+        //the last player gets the highest score in every round
+        public static Match CreateSampleMatch(int matchId, DateTime playedOn, List<Party> players)
+        {
+            Match match = new Match()
+            {
+                MatchId = matchId,
+                Rounds = new List<Round>()
+                {
+                    new Round() { Series = new List<Series>() },
+                    new Round() { Series = new List<Series>() }
+                },
+                PlayedOn = playedOn
+            };
+
+            for (short i = 0; i < players.Count; i++)
+            {
+                foreach (Round round in match.Rounds)
+                    round.Series.Add(new Series() { Player = players[i], Score = i });
+            }
+
+            return match;
+        }
+    }
+}
