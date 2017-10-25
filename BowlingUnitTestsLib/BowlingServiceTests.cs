7b214b2ffc088081365c01743a70d04dd89ab270
aeklundh <eklundh.albin@gmail.com>
2017-10-25 15:10:51 +0200

    add fake bowling repository & standard match test

--- BowlingUnitTestsLib/BowlingServiceTests.cs
+++ BowlingUnitTestsLib/BowlingServiceTests.cs
@@ -1,9 +1,41 @@
+using AccountabilityLib.Models;
+using BowlingLib.Interfaces;
+using BowlingLib.Models;
+using BowlingLib.Services;
+using BowlingUnitTestsLib.Repositories;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace BowlingUnitTestsLib
 {
     public class BowlingServiceTests
     {
+        [Theory]
+        [InlineData(2, 2, 1, 2)]
+        [InlineData(3, 2, 2, 2)]
+        [InlineData(2, 4, 1, 2)]
+        public void StandardMatch(int playerAmount, int laneAmount, int expectedOnFirstLane, int expectedLanes)
+        {
+            //Assemble
+            IBowlingRepository fakeProvider = new FakeBowlingRepository();
+            BowlingService sut = new BowlingService(fakeProvider);
+            ICollection<Party> players = new List<Party>();
+            ICollection<Lane> lanes = new List<Lane>();
+
+            for (int i = 0; i < playerAmount; i++)
+                players.Add(new Party());
+
+            for (int i = 0; i < laneAmount; i++)
+                lanes.Add(new Lane());
+
+            //Act
+            Match result = sut.CreateStandardMatch(players, lanes).Result;
+
+            //Assert
+            Assert.Equal(result.Rounds.First().Series.Count, expectedOnFirstLane);
+            Assert.Equal(result.Rounds.GroupBy(x => x.Lane).Count(), expectedLanes);
+        }
     }
 }
---
+++ BowlingUnitTestsLib/Repositories/FakeBowlingRepository.cs
@@ -0,0 +1,65 @@
+﻿using BowlingLib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BowlingLib.Models;
+using System.Threading.Tasks;
+
+namespace BowlingUnitTestsLib.Repositories
+{
+    public class FakeBowlingRepository : IBowlingRepository
+    {
+        private List<Match> _matches { get; set; } = new List<Match>();
+
+        public async Task<Match> AddMatch(Match match)
+        {
+            _matches.Add(match);
+            return await Task.FromResult(match);
+        }
+
+        public Task<Competition> CreateCompetition(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Match> CreateEmptyMatch()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Round> CreateRound(Match match)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Lane>> GetAllLanes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Competition> GetCompetition(int competitionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Lane> GetLane(int laneId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Match> GetMatch(int matchId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Round> GetRound(int roundId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Series> GetSeries(int seriesId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
