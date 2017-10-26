d098c18ed2716fd29619c26f74b5afb237a42c23
aeklundh <eklundh.albin@gmail.com>
2017-10-26 13:36:02 +0200

    add integration test for party creation & necessary assembly reference

---
+++ BowlingIntegrationTestsLib/SqlPartyRepositoryTests.cs
@@ -0,0 +1,28 @@
+﻿using BowlingDbLib;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BowlingIntegrationTestsLib
+{
+    public class SqlPartyRepositoryTests
+    {
+        [Fact]
+        public void CanCreateNewParty()
+        {
+            //Assemble
+            BowlingDbContext context = new BowlingDbContextFactory().CreateDbContext(new string[] { });
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            SqlPartyRepository sut = new SqlPartyRepository(context);
+
+            //Act
+            sut.CreateParty("aaa", "AAA").Wait();
+
+            //Assert
+            Assert.NotNull(sut.GetParty("AAA").Result);
+        }
+    }
+}
