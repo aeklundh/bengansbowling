b07fc89fdeeff0d495d0782c80cd7bce4c0d3ae5
aeklundh <eklundh.albin@gmail.com>
2017-10-26 13:33:58 +0200

    add factory for dbcontext

---
+++ BowlingDbLib/BowlingDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingDbLib
+{
+    public class BowlingDbContextFactory : IDesignTimeDbContextFactory<BowlingDbContext>
+    {
+        public BowlingDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<BowlingDbContext>().UseInMemoryDatabase("database");
+            return new BowlingDbContext(builder.Options);
+        }
+    }
+}
