a8b9205807ad196754244dcc900a6dee5fb15753
aeklundh <eklundh.albin@gmail.com>
2017-11-06 13:03:39 +0100

    add player registration and test

--- BowlingIntegrationTestsLib/SqlPartyRepositoryTests.cs
+++ BowlingIntegrationTestsLib/SqlPartyRepositoryTests.cs
@@ -1,18 +1,21 @@
-﻿using BowlingDbLib;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AccountabilityLib.Interfaces;
+using AccountabilityLib.Models;
+using BowlingDbLib;
+using BowlingLib.Services;
 using Xunit;
 
 namespace BowlingIntegrationTestsLib
 {
     public class SqlPartyRepositoryTests
     {
+        private string _inMemoryDbName {
+            get { return "SqlPartyRepositoryTestsDb"; }
+        }
+
         [Fact]
         public void CanCreateNewParty()
         {
-            using (BowlingDbContext context = new BowlingDbContextFactory().CreateInMemoryDbContext("SqlPartyRepositoryTestsDb"))
+            using (BowlingDbContext context = new BowlingDbContextFactory().CreateInMemoryDbContext(_inMemoryDbName))
             {
                 //Assemble
                 SqlPartyRepository sut = new SqlPartyRepository(context);
@@ -22,6 +25,26 @@ namespace BowlingIntegrationTestsLib
 
                 //Assert
                 Assert.NotNull(sut.GetParty("AAA").Result);
+            }
+        }
+
+        [Fact]
+        public void CanCreateAndGetPlayers()
+        {
+            using (BowlingDbContext context = new BowlingDbContextFactory().CreateInMemoryDbContext(_inMemoryDbName))
+            {
+                //Assemble
+                IPartyRepository partyRepository = new SqlPartyRepository(context);
+                partyRepository.CreateParty("bengan", "bengansLegalId");
+                partyRepository.CreateAccountabilityType("Customer");
+                PartySystem sut = new PartySystem(partyRepository, "bengansLegalId");
+
+                //Act
+                Party result = sut.RegisterNewPlayer("aaa", "AAA").Result;
+
+                //Assert
+                Assert.NotNull(result);
+                Assert.True(result.Commissions.Count == 1);
             }
         }
     }
---
+++ BowlingLib/Services/PartySystem.cs
@@ -0,0 +1,37 @@
+﻿using AccountabilityLib.Interfaces;
+using AccountabilityLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingLib.Services
+{
+    public class PartySystem
+    {
+        private readonly IPartyRepository _partyRepository;
+        private readonly string _accountabilityOrigin;
+
+        public PartySystem(IPartyRepository partyRepository, string accountabilityOrigin)
+        {
+            _partyRepository = partyRepository;
+            _accountabilityOrigin = accountabilityOrigin;
+        }
+
+        public async Task<Party> RegisterNewPlayer(string name, string legalId)
+        {
+            Party newPlayer = await _partyRepository.CreateParty(name, legalId);
+            Party origin = await _partyRepository.GetParty(_accountabilityOrigin);
+            AccountabilityType type = await _partyRepository.GetAccountabilityType("Customer");
+            if (type != null && origin != null)
+            {
+                await _partyRepository.CreateAccountability(newPlayer, origin, type);
+                return newPlayer;
+            }
+            else
+            {
+                throw new NullReferenceException();
+            }
+        }
+    }
+}
