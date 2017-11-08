4c37969d5069f6dffa2a170c3000951a8773d596
aeklundh <eklundh.albin@gmail.com>
2017-11-08 19:39:50 +0100

    add includes in repository for SQL db implementations

--- BowlingDbLib/SqlBowlingRepository.cs
+++ BowlingDbLib/SqlBowlingRepository.cs
@@ -46,7 +46,12 @@ namespace BowlingDbLib
 
         public async Task<Competition> GetCompetition(int competitionId)
         {
-            return await _context.Competitions.SingleOrDefaultAsync(x => x.CompetitionId == competitionId);
+            return await _context.Competitions
+                .Include(x => x.Matches)
+                .ThenInclude(x => x.Rounds)
+                .ThenInclude(x => x.Series)
+                .ThenInclude(x => x.Player)
+                .SingleOrDefaultAsync(x => x.CompetitionId == competitionId);
         }
 
         public async Task<Lane> GetLane(int laneId)
@@ -56,12 +61,21 @@ namespace BowlingDbLib
 
         public async Task<Match> GetMatch(int matchId)
         {
-            return await _context.Matches.SingleOrDefaultAsync(x => x.MatchId == matchId);
+            return await _context.Matches
+                .Include(x => x.Competition)
+                .Include(x => x.Rounds)
+                .ThenInclude(x => x.Series)
+                .ThenInclude(x => x.Player)
+                .SingleOrDefaultAsync(x => x.MatchId == matchId);
         }
 
         public async Task<Round> GetRound(int roundId)
         {
-            return await _context.Rounds.SingleOrDefaultAsync(x => x.RoundId == roundId);
+            return await _context.Rounds
+                .Include(x => x.Series)
+                .ThenInclude(x => x.Player)
+                .Include(x => x.Match)
+                .SingleOrDefaultAsync(x => x.RoundId == roundId);
         }
 
         public async Task<Series> GetSeries(int seriesId)
@@ -79,7 +93,12 @@ namespace BowlingDbLib
 
         public async Task<ICollection<Match>> GetMatchesByYear(int year)
         {
-            return await _context.Matches.Where(x => x.PlayedOn.Year == year).ToListAsync();
+            return await _context.Matches
+                .Include(x => x.Competition)
+                .Include(x => x.Rounds)
+                .ThenInclude(x => x.Series)
+                .ThenInclude(x => x.Player)
+                .Where(x => x.PlayedOn.Year == year).ToListAsync();
         }
 
         public async Task<Lane> CreateLane(string name)
